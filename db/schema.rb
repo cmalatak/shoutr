78c3a3861ecbdae4bd88dece5bcd6f840464979e
cmalatak <cass@Cassies-MacBook-Pro.local>
2025-01-27 11:31:17 -0500

    runs migration to add shouts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_21_154210) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_27_160824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "shouts", force: :cascade do |t|
+    t.string "body", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shouts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,4 +35,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_21_154210) do
     t.index ["remember_token"], name: "index_users_on_remember_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "shouts", "users"
 end
