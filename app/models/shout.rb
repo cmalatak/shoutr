7f265685343824ffef1736ff3fcd8694c338967c
cmalatak <cass@Cassies-MacBook-Pro.local>
2025-01-27 11:53:52 -0500

    adds partial for shouts to clean up rendering and make something reusable

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -3,4 +3,8 @@ class Shout < ApplicationRecord
 
   validates :body, presence: true, length: { in: 1..100 } # Todo update later to like 288 or something
   validates :user, presence: true
+
+  default_scope { order(created_at: :desc) }
+
+  delegate :username, to: :user
 end
