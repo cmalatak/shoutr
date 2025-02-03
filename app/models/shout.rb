d9c45b9a6718f83abb4cea031556e1a690c5b904
cmalatak <cass@Cassies-MacBook-Pro.local>
2025-02-03 15:59:56 -0500

    Refactor app to support polymorphic relationship

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,7 +1,7 @@
 class Shout < ApplicationRecord
   belongs_to :user
+  belongs_to :content, polymorphic: true
 
-  validates :body, presence: true, length: { in: 1..100 } # Todo update later to like 288 or something
   validates :user, presence: true
 
   default_scope { order(created_at: :desc) }
--- app/models/text_shout.rb
+++ app/models/text_shout.rb
@@ -1,2 +1,3 @@
 class TextShout < ApplicationRecord
+  validates :body, presence: true, length: { in: 1..100 } # Todo update later to like 288 or something
 end
