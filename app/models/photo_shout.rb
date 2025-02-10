f5a8f7c525c381069e678fe8d3b957008c43efdb
cmalatak <cass@Cassies-MacBook-Pro.local>
2025-02-10 11:45:56 -0500

    successfully adds photo shouts ... they are large lol

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -1,3 +1,9 @@
 class PhotoShout < ApplicationRecord
-  has_one_attached :image
+  ACCEPTED_CONTENT_TYPES = [ "image/jpeg", "image/gif", "image/png", "image/webp" ].freeze
+
+
+  has_one_attached :image # , styles: { thumb: "200x200" } Need to look into how to do this differently
+
+  validates :image, content_type: ACCEPTED_CONTENT_TYPES # only allows JPEG, GIF, PNG, and WebP images
+  validates :image, size: { less_than_or_equal_to: 1.megabytes } # restricts the file size to <= 1MB
 end
