73f88c8c332f3f963724b76a1cb4214449593744
cmalatak <cass@Cassies-MacBook-Pro.local>
2025-02-24 11:29:26 -0500

    migration to create following relationships table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_18_154341) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_24_160854) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_18_154341) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "following_relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "followed_user_id"], name: "index_follower_following_relationship", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
@@ -89,6 +97,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_18_154341) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "following_relationships", "users", column: "followed_user_id"
+  add_foreign_key "following_relationships", "users", column: "follower_id"
   add_foreign_key "likes", "shouts"
   add_foreign_key "likes", "users"
   add_foreign_key "shouts", "users"
